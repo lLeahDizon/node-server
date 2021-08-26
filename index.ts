1f800b183a11bd42b378fedd234cba9921ebf170
lLeahDizon <516330797@qq.com>
2021-08-26 20:24:47 +0800

    feature: 用 Node.js 获取请求内容

--- index.ts
+++ index.ts
@@ -1,10 +1,28 @@
 import * as http from 'http';
+import {IncomingMessage, ServerResponse} from 'http';
 
 const server = http.createServer();
 
-server.on('request', (request, response) => {
-  console.log('有人请求了');
-  response.end('hi');
+server.on('request', (request: IncomingMessage, response: ServerResponse) => {
+  console.log('request.method');
+  console.log(request.method);
+  console.log('request.url');
+  console.log(request.url);
+  console.log('request.headers');
+  console.log(request.headers);
+
+  const array = [];
+  request.on('data', (chunk) => {
+    array.push(chunk);
+  });
+
+  request.on('end', () => {
+    const body = Buffer.concat(array).toString();
+    console.log('body');
+    console.log(body);
+    response.end('hi');
+  });
 });
 
+// 开始监听 8888 端口
 server.listen(8888);
