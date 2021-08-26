7586f1ccdb2fa56cb4ce804a7c312784de1a6e49
lLeahDizon <516330797@qq.com>
2021-08-26 19:29:02 +0800

    feature: 初始化库

---
+++ index.ts
@@ -0,0 +1,10 @@
+import * as http from 'http';
+
+const server = http.createServer();
+
+server.on('request', (request, response) => {
+  console.log('有人请求了');
+  response.end('hi');
+});
+
+server.listen(8888);
