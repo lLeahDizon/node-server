133a5286411e5c89f7157235db5b4a614f5f2888
lLeahDizon <516330797@qq.com>
2021-08-27 16:57:58 +0800

    feature: 处理 url 查询参数

--- index.ts
+++ index.ts
@@ -2,13 +2,15 @@ import * as http from 'http';
 import {IncomingMessage, ServerResponse} from 'http';
 import * as fs from 'fs';
 import * as p from 'path';
+import * as url from 'url';
 
 const server = http.createServer();
 const publicDir = p.resolve(__dirname, 'public');
 
 server.on('request', (request: IncomingMessage, response: ServerResponse) => {
-  const {method, url, headers} = request;
-  switch (url) {
+  const {method, url: path, headers} = request;
+  const {pathname, search} = url.parse(path);
+  switch (pathname) {
     case '/index.html':
       response.setHeader('Content-Type', 'text/html; charset=utf-8');
       fs.readFile(p.resolve(publicDir, 'index.html'), (error, data) => {
@@ -29,6 +31,10 @@ server.on('request', (request: IncomingMessage, response: ServerResponse) => {
         if (error) throw error;
         response.end(data.toString());
       });
+      break;
+    default:
+      response.statusCode = 404;
+      response.end();
       break;
   }
 });
