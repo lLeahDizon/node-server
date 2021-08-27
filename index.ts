62df64c600cad7157a192d1e7fa28dc8f72395ba
lLeahDizon <516330797@qq.com>
2021-08-27 15:49:40 +0800

    feature: 根据 url 不同返回文件

--- index.ts
+++ index.ts
@@ -1,32 +1,36 @@
 import * as http from 'http';
 import {IncomingMessage, ServerResponse} from 'http';
+import * as fs from 'fs';
+import * as p from 'path';
 
 const server = http.createServer();
+const publicDir = p.resolve(__dirname, 'public');
 
 server.on('request', (request: IncomingMessage, response: ServerResponse) => {
-  console.log('request.method');
-  console.log(request.method);
-  console.log('request.url');
-  console.log(request.url);
-  console.log('request.headers');
-  console.log(request.headers);
-
-  const array = [];
-  request.on('data', (chunk) => {
-    array.push(chunk);
-  });
-
-  request.on('end', () => {
-    const body = Buffer.concat(array).toString();
-    console.log('body');
-    console.log(body);
-
-    response.statusCode = 404;
-    response.setHeader('X-lemon', `I'm Lemon`);
-    response.setHeader('Content-Type', 'image/png');
-    response.write(ImageData);
-    response.end();
-  });
+  const {method, url, headers} = request;
+  switch (url) {
+    case '/index.html':
+      response.setHeader('Content-Type', 'text/html; charset=utf-8');
+      fs.readFile(p.resolve(publicDir, 'index.html'), (error, data) => {
+        if (error) throw error;
+        response.end(data.toString());
+      });
+      break;
+    case '/style.css':
+      response.setHeader('Content-Type', 'text/css; charset=utf-8');
+      fs.readFile(p.resolve(publicDir, 'style.css'), (error, data) => {
+        if (error) throw error;
+        response.end(data.toString());
+      });
+      break;
+    case '/main.js':
+      response.setHeader('Content-Type', 'text/javascript; charset=utf-8');
+      fs.readFile(p.resolve(publicDir, 'main.js'), (error, data) => {
+        if (error) throw error;
+        response.end(data.toString());
+      });
+      break;
+  }
 });
 
 // 开始监听 8888 端口
