098c8cc017a6bdaa17601d7c8e5cd3dfda5b7f79
lLeahDizon <516330797@qq.com>
2021-08-28 14:48:48 +0800

    feature: 处理不存在的文件

--- index.ts
+++ index.ts
@@ -11,13 +11,27 @@ server.on('request', (request: IncomingMessage, response: ServerResponse) => {
   const {method, url: path, headers} = request;
   const {pathname, search} = url.parse(path);
   // response.setHeader('Content-Type', 'text/html; charset=utf-8');
-  const filename = pathname.substr(1);
+  let filename = pathname.substr(1);
+  if (filename === '') {
+    filename = 'index.html';
+  }
   fs.readFile(p.resolve(publicDir, filename), (error, data) => {
     if (error) {
-      response.statusCode = 404;
-      response.end('你要的文件不存在');
+      console.log(error);
+      if (error.errno === -2) {
+        response.statusCode = 404;
+        fs.readFile(p.resolve(publicDir, '404.html'), (error, data) => {
+          response.end(data);
+        });
+      } else if (error.errno === -21) {
+        response.statusCode = 403;
+        response.end('无权查看目录内容');
+      } else {
+        response.statusCode = 500;
+        response.end('服务器繁忙，请稍后再试');
+      }
     } else {
-      response.end(data.toString());
+      response.end(data);
     }
   });
 });
